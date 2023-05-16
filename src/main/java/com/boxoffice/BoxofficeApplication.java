61984f2834c87fe90f1fffb7a5888205b0e793f0
sushin <iss0416@naver.com>
2023-05-16 23:10:30 +0900

    프로젝트 생성

---
+++ src/main/java/com/boxoffice/BoxofficeApplication.java
@@ -0,0 +1,13 @@
+package com.boxoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxofficeApplication.class, args);
+	}
+
+}
